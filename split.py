8eef4b39ec4df6ad7319404eee55690ea37b0e8f
Joep van Gassel <jvgassel@gmail.com>
2018-09-04 08:19:46 +0200

    Update split.py

--- split.py
+++ split.py
@@ -1,16 +1,13 @@
 from PyPDF2 import PdfFileWriter, PdfFileReader
 
-inputpdf = PdfFileReader(open("rapport.pdf", "rb"))
+inputpdf = PdfFileReader(open("robje.pdf", "rb"))
 
 with open('regcode.txt') as f:
     lines = [line.rstrip('\n') for line in open('regcode.txt')]
 	
-i = 0
-while i < len(lines):
+output = PdfFileWriter()
+for j in range(inputpdf.numPages):
 	output = PdfFileWriter()
-	for j in range(inputpdf.numPages):
-		output = PdfFileWriter()
-		output.addPage(inputpdf.getPage(i))
-		outputStream = file(lines[i] + ".pdf", "wb")
-		output.write(outputStream)
-	i += 1
+	output.addPage(inputpdf.getPage(j))
+	outputStream = file(lines[j] + ".pdf", "wb")
+	output.write(outputStream)
